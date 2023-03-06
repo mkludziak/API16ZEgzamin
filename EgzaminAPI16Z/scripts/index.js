6ecad59ce4eee247b9bbbe267aac306bf2c8fc79
mkludziak <119815038+mkludziak@users.noreply.github.com>
2023-03-06 13:15:41 +0100

    Add files via upload

---
+++ EgzaminAPI16Z/scripts/index.js
@@ -0,0 +1,5 @@
+//console.log("Martyna Kłudziak-Ciurej");
+//document.write("Martyna");
+
+document.getElementById("name").innerHTML = "Martyna Kłudziak-Ciurej";
+//document.getElementById("lastname").innerHTML = "Kłudziak-Ciurej";
